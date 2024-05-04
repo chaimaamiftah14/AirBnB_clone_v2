b436816a4f795c9cf6d21679d12b5dd954d0a814
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-05-05 00:23:58 +0100

    Refactor HBNBCommand's create method to support optional attributes

--- console.py
+++ console.py
@@ -78,7 +78,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is '}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -119,17 +119,45 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class"""
+        """Create an object of any class with optional attributes."""
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+
+        args_list = args.split()
+        class_name = args_list[0]
+        
+        if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+        
+        # Check for initialization parameters
+        if len(args_list) > 1:
+            # Create a dictionary to hold the attribute-value pairs
+            attr_dict = {}
+            for param in args_list[1:]:
+                if "=" in param:
+                    key, val = param.split("=", 1)
+                    # Remove leading/trailing quotes and replace underscores with spaces
+                    if val[0] == '"' and val[-1] == '"':
+                        val = val[1:-1].replace('_', ' ').replace('\"', '\"')
+                    # Convert to float or int if applicable
+                    try:
+                        if '.' in val:
+                            val = float(val)
+                        else:
+                            val = int(val)
+                    except ValueError:
+                        pass  # keep the string if it cannot be converted
+                    attr_dict[key] = val
+
+        # Create an instance and set attributes
+        new_instance = HBNBCommand.classes[class_name]()
+        for key, val in attr_dict.items():
+            if hasattr(new_instance, key):
+                setattr(new_instance, key, val)
+
+        new_instance.save()  # Save the new instance to storage
 
     def help_create(self):
         """ Help information for the create method """
@@ -277,7 +305,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -285,10 +313,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
